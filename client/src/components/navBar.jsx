22ddc82e16d8ff0817cdf001a792f58e2ebcb949
Avila-Pato <p.avilaf1998@gmail.com>
2024-08-01 20:06:26 -0400

    Agregando logica al sidebar y navbar pero que funcione

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import {
   LightModeOutlined,
   DarkModeOutlined,
   Menu as MenuIcon,
   Search,
-  SettingsOutlined,
-  ArrowDropDownOutlined,
+  // SettingsOutlined,
+  // ArrowDropDownOutlined,
 } from "@mui/icons-material";
 import FlexBetween from "./flexBetween";
 
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
-import profileImage from "assets/152195893.jpg";
+// import profileImage from "assets/152195893.jpg";
 import { useTheme } from "@mui/material";
 import { AppBar, IconButton, InputBase } from "@mui/material";
 import Tollbar from "@mui/material/Toolbar";
 
-const NavBar = () => {
+const NavBar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
@@ -31,7 +31,7 @@ const NavBar = () => {
       <Tollbar sx={{ justifyContent: "space-between" }}>
         {/* Lado izquierdo */}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
@@ -45,6 +45,16 @@ const NavBar = () => {
               <Search />
             </IconButton>
           </FlexBetween>
+        </FlexBetween>
+        {/* LADO DERE DERECHO  del modo blue y white*/}
+        <FlexBetween>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
         </FlexBetween>
       </Tollbar>
     </AppBar>
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,13 +3,29 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import NavBar from "components/navBar";
+import SideBar from "components/sideBar";
 
 const Layout = () => {
-  // Tu código aquí
+  const isNonMobile = useMediaQuery("(min-width: 600px");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <Box width={"100%"} height={"100vh"}>
+    <Box
+      display={isNonMobile ? "flex" : "block"}
+      width={"100%"}
+      height={"100vh"}
+    >
+      <SideBar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-        <NavBar />
+        <NavBar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
